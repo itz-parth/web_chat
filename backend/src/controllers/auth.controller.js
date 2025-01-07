1cadaf4d7b2f40e1f5ee1261280fb1512c1467c1
itz-parth <parthapm2018@gmail.com>
2025-01-07 23:14:44 +0530

    signup backend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,31 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs"
 
+export const signup = ("/signup", async (req, res) => {
+    const { fullName, email, password} = req.body;
+    try{
+        if(password.length < 6){
+            return res.status(404).json({ message: "Password must be greater than 6 characters" });
+        }
 
-export const signup = ("/signup", (req, res) => {
-    res.send("signup route")
+        const user = await User.findOne(email);
+        if(user){
+            return res.status(404).json({ message: "Email already exist" });
+        }
+
+        const salt = await bcrypt.gensalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new User({
+            fullName,
+            email,
+            password:hashedPassword 
+        })
+
+    } catch (e){
+        console.log("Error in signup", e.message);
+        return res.status(500).json({ message: "Internal error" });
+    }
 })
 
 export const login = ("/login", (req, res) => {
